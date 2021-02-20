560fb6c9339a83230adef7eae489537ca2da0c31
Karyna Avramenko <karinmia99avr@gmail.com>
2021-02-20 21:06:15 +0200

    Register User model in admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('phone', 'full_name',)
+    search_fields = ('full_name', 'phone')
+    list_filter = ['is_staff', 'is_active']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('full_name', 'phone',)}
+         ),
+    )
+    fieldsets = (
+        ('Personal info', {'fields': ('full_name', 'phone',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    ordering = ('phone',)
+
+
+admin.site.register(User, CustomUserAdmin)
